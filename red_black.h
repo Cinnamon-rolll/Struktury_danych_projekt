ca8bbc961d39a98f33a4bf718b6fa7deb85de3a0
Wiktoria Sawicka <129857314+Wiktoria-S@users.noreply.github.com>
2023-05-12 23:15:13 +0200

    Update red_black.h

--- red_black.h
+++ red_black.h
@@ -1,35 +1,32 @@
-//might change
-
 #pragma once
-#include <iostream>
+#define RED 0
+#define BLACK 1
 
-using namespace std;
-
-class Node
+struct Node
 {
-public:
-	Node()
-	{
-		this->data = data;
-		this->key = key;
-		this->color = color;
-		left = nullptr;
-		right = nullptr;
-		parent = nullptr;
-		root = nullptr;
-	};
-	~Node(){};
-
-	void change_color(Node* node);
-	Node* left_rotate(Node* node);
-	Node* right_rotate(Node* node);
-
-private:
 	int color;
 	int data;
-	int key;
 	Node* left;
 	Node* right;
 	Node* parent;
-	Node* root;
+};
+
+class Red_Black_Tree
+{
+private:
+	struct Node* nill;
+	struct Node* root;
+	struct Node* temp;
+public:
+	void rotate_left(Node*node_x);
+	void rotate_right(Node*node_y);
+	void insert(struct Node* x, struct Node* y, struct Node* temp);
+	void tree_print(struct Node* node, struct Node* nill);
+	void insertion_fix_up(Node*&node);
+	int max_depth(Node* n);
+	void nill_init(Node*& n);
+	void temp_init(Node*&, int);
+	void menu();
+	Red_Black_Tree(){};
+	~Red_Black_Tree(){};
 };
