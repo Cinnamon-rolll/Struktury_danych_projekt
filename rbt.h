42fef6f558ec0651628caefa992678f5e3129d84
Wiktoria Sawicka <129857314+Wiktoria-S@users.noreply.github.com>
2023-05-26 15:49:56 +0200

    Update rbt.h

--- rbt.h
+++ rbt.h
@@ -21,12 +21,20 @@ public:
 	void rotate_left(Node*node_x);
 	void rotate_right(Node*node_y);
 	void insert(struct Node* x, struct Node* y, struct Node* temp);
-	void tree_print(struct Node* node, struct Node* nill);
+	bool insert_check(Node* node, int dane);
+	void print_tree(struct Node* aux, struct Node* nill);
 	void insertion_fix_up(Node*&node);
+	void delete_node(struct Node* node, struct Node* x, struct Node* y);
+	void deletion_fix_up(struct Node* x, struct Node* y);
 	int max_depth(Node* n);
 	void nill_init(Node*& n);
-	void temp_init(Node*&, int);
+	void temp_init(Node*& n, int);
+	void print_elements(Node* node, Node* nill);
+	Node* successor_init(Node*& n);
+	void deletion_driver(Node* n, int dane);
+
 	void menu();
 	Red_Black_Tree(){};
-	~Red_Black_Tree(){};
+	~Red_Black_Tree() {};
 };
+
