e73d5927943c9876055f00b9df91fb4b2863af57
Wiktoria Sawicka <129857314+Wiktoria-S@users.noreply.github.com>
2023-06-07 20:08:16 +0200

    Update rbt.h

--- rbt.h
+++ rbt.h
@@ -18,23 +18,25 @@ private:
 	struct Node* root;
 	struct Node* temp;
 public:
-	void rotate_left(Node*node_x);
-	void rotate_right(Node*node_y);
-	void insert(struct Node* x, struct Node* y, struct Node* temp);
+	void rotate_left(Node* node_x);
+	void rotate_right(Node* node_y);
+	void insert(Node* x, Node* y, Node* temp);
 	bool insert_check(Node* node, int dane);
-	void print_tree(struct Node* aux, struct Node* nill);
-	void insertion_fix_up(Node*&node);
-	void delete_node(struct Node* node, struct Node* x, struct Node* y);
-	void deletion_fix_up(struct Node* x, struct Node* y);
+	void print_tree(Node* node, Node* nill);
+	void insertion_fix_up(Node* node);
+	void delete_node(Node* node, Node* x, Node* y);
+	void deletion_fix_up(Node* x, Node* y);
 	int max_depth(Node* n);
-	void nill_init(Node*& n);
-	void temp_init(Node*& n, int);
+	Node* nill_init(Node* n);
+	Node* temp_init(Node* n, int);
 	void print_elements(Node* node, Node* nill);
-	Node* successor_init(Node*& n);
-	void deletion_driver(Node* n, int dane);
-
+	Node* successor_init(Node* node);
+	void deletion_driver(Node* node, int dane);
+	void tree_move(Node* node,  Node* child_node);
 	void menu();
-	Red_Black_Tree(){};
+
+	void generate(int* tab, int amount);
+
+	Red_Black_Tree() {};
 	~Red_Black_Tree() {};
 };
-
